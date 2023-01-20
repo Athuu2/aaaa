5c3024c3f31332dc14a3aea169d1ab1deaeb5abd
Athuu2 <115910053+Athuu2@users.noreply.github.com>
2023-01-19 22:45:23 -0300

    Update define_util.cpp

--- apex_silentaim/define_util.cpp
+++ apex_silentaim/define_util.cpp
@@ -4,8 +4,8 @@
 #include "Memory.cpp"
 #include "Main.cpp"
 
-#define MONITOR_WIDTH 1600
-#define MONITOR_HEIGHT 900
+#define MONITOR_WIDTH 1920
+#define MONITOR_HEIGHT 1080
 
 struct my_vector {
     float x;
